4e2a04ceaad209cdf1c2c029ef2ad0e2871cbd8d
aritrax <aritrapaul2610@gmail.com>
2026-08-19 23:26:31 +0530

    package.json added HTML reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
 
   /* Configure projects for major browsers */
   projects: [
